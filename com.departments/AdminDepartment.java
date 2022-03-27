17948013b1b0a46a61775962a42019908fbbde49
raghavjnvk <96766510+raghavjnvk@users.noreply.github.com>
2022-03-27 23:18:52 +0530

    AdminDepartment.java
    AdminDepartment created.

---
+++ com.departments/AdminDepartment.java
@@ -0,0 +1,22 @@
+
+package com.departments;
+
+public class AdminDepartment extends SuperDepartment {
+
+    String departmentName() {
+
+        return " Admin Department ";
+    }
+
+    String getTodaysWork() {
+
+
+        return " Complete your documents submission ";
+    }
+
+    String getWorkDeadline() {
+
+
+        return " Complete by EOD ";
+    }
+}
