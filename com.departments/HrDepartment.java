c72fb3e69224a107f377832eaf1850925e579b20
raghavjnvk <96766510+raghavjnvk@users.noreply.github.com>
2022-03-27 23:22:31 +0530

    HrDepartment.java
    Created HRDepartment.java file.

---
+++ com.departments/HrDepartment.java
@@ -0,0 +1,20 @@
+package com.departments;
+
+public class HrDepartment extends SuperDepartment {
+
+    String departmentName() {
+        return " HR Department ";
+    }
+
+    String getTodaysWork() {
+        return " Fill today’s timesheet and mark your attendance ";
+    }
+
+    String getWorkDeadline() {
+        return " Complete by EOD ";
+    }
+
+    String doActivity() {
+        return " team Lunch ";
+    }
+}
