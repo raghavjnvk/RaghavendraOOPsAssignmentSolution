b6192e05162ac0f3207489c974cbe7c25a811446
raghavjnvk <96766510+raghavjnvk@users.noreply.github.com>
2022-03-27 23:25:34 +0530

    TechDepartment.java
    TechDepartment.java file created.

---
+++ com.departments/TechDepartment.java
@@ -0,0 +1,21 @@
+package com.departments;
+
+public class TechDepartment extends SuperDepartment {
+
+    String departmentName() {
+        return " Tech Department ";
+    }
+
+    String getTodaysWork() {
+        return " Complete coding of Module 1 ";
+    }
+
+    String getWorkDeadline() {
+        return " Complete by EOD ";
+    }
+
+    String getTechStackInformation() {
+        return " Core Java ";
+    }
+
+}
